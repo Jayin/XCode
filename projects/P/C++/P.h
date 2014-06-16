b2a7fa6e007fd5e3c63568a46621e4c6e53c82b6
Jayin Ton <tonjayin@gmail.com>
2014-06-16 16:15:07 +0800

    [enhance]int support & deal with the key not found

--- projects/P/C++/P.h
+++ projects/P/C++/P.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <list>
 #include <map>
+#include <cmath>
+#include <cstdio>
 using namespace std;
 
 class Token{
@@ -17,6 +19,15 @@ public :
 	int end;
 };
 
+static const char* itostirng(int integer) {
+	if(integer ==0)
+		return "0";
+	int len = (int) (log10(abs(integer)) + 1);
+	char *tmp = new char[len];
+	sprintf(tmp, "%d", integer);
+	return tmp;
+}
+
 
 class P {
 public:
@@ -24,6 +35,7 @@ public:
 	virtual ~P();
 	static P from(const char* origin);
 	P put(const char* key,const char* value);
+	P put(const char* key,int value);
 	const char* format();
 
 private :
@@ -49,6 +61,12 @@ P P::put(const char* key,const char* value){
 	mKV[key] = value;
 	return *this;
 }
+
+P P::put(const char* key,int value){
+	return this->put(key,itostirng(value));
+}
+
+
 
 //直接来个try  catch 说明格式有错
 list<Token> getTokens(string str) {
@@ -93,11 +111,14 @@ const char* P::format(){
 	//to replace the toke by strcat
 	int offset = 0;
 	for(list<Token>::iterator it =tokens.begin();it != tokens.end();it++){
+		// not found the key
+		if(mKV.find(it->key) == mKV.end()){
+			continue;
+		}
 		int replace_len = it->end - it->start +1;
 		mStr.replace(it->start + offset,replace_len,mKV[it->key]);
 		offset  += mKV[it->key].size() - replace_len ;
 	}
-
 	return mStr.data();
 }
 
--- projects/P/C++/P_test.cpp
+++ projects/P/C++/P_test.cpp
@@ -1,11 +1,16 @@
 #include "P.h"
 #include <iostream>
 #include <list>
+#include <string>
 using namespace std;
 int main() {
-	const char * str = "I am { name } and user_id = {user_id} with alias {name}.";
-	const char* s = P::from(str).put("name", "Jayin").put("user_id","3112002722").format();
-	cout << s;
+	const char * str =
+			"I am { name } and user_id = {user_id} with alias {name}.";
+	const char* s = P::from(str).put("name", "Jayin").put("user_id",
+			"3112002722").format();
+	cout << s << endl;
+	s = P::from(str).put("name1", "Jayin").put("user_id", 1234567).format();
+	cout << s<<endl;
 	return 0;
 }
 
