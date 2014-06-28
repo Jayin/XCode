1a96ada180394e0dd241434820094839a2b1c115
jianzhiyao <739319867@qq.com>
2014-06-28 20:58:17 +0800

    add judgement of paramters

--- projects/array-differ/php/arraydiffer.php
+++ projects/array-differ/php/arraydiffer.php
@@ -1,28 +1,39 @@
 <?php
 function array_differ(){
-	$num=func_num_args();
-	if($num<=0)
-	throw new Exception("$num paramters given while array_differ function need 2 array as paramters at least");
-	$lists=func_get_args();
-	$U=array();
-	foreach($lists as $k=>$v)
-	{
-		if($k==0)
+	try{
+		$num=func_num_args();
+		$e=new Exception("$num paramters  given while array_differ function need 2 array as paramters at least");
+		$not_array_exception=new  Exception("all paramters of function array_differ should be array");
+		if($num<=0)
+			throw $e;
+		$lists=func_get_args();
+		$U=array();
+		foreach($lists as $k=>$v)
 		{
-			$U=$v;
-		}
-		else
-		{
-			$a=$v;
-			foreach($a as $ka=>$va)
+			if(!is_array($lists[$k]))
+				throw $not_array_exception;
+			if($k==0)
 			{
-				while(!(($delkey=array_search($va,$U))===false))
+				$U=$v;
+			}
+			else
+			{
+				$a=$v;
+				foreach($a as $ka=>$va)
 				{
-						unset($U[$delkey]);
+					while(!(($delkey=array_search($va,$U))===false))
+					{
+							unset($U[$delkey]);
+					}
 				}
 			}
 		}
+		return $U;
 	}
-	return $U;
+	catch(Exception $e)
+	{
+		echo $e->getMessage()." in file ".__FILE__." in line ".$e->getLine();
+		exit;
+	}
 }
 ?>
--- projects/array-differ/php/arraydiffer_test.php
+++ projects/array-differ/php/arraydiffer_test.php
@@ -1,4 +1,4 @@
 <?php
 	require_once "arraydiffer.php";
-	var_dump(array_differ(array("green", "red", "blue", "red"),array("green", "yellow", "red")));
+	var_dump(array_differ(array("green","red","blue","red"),array("red","yellow","green")));
 ?>
